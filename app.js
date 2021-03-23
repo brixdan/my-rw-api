020b1182d449d8d54f8b4af00c4baf2807b5bf36
brixdan <brixdan@gmail.com>
2021-03-23 12:31:45 +0300

    Additinal corses off

--- app.js
+++ app.js
@@ -44,13 +44,7 @@ if(isProduction){
 mongoose.connect("mongodb+srv://brixdan:MOz0x9c8v7@cluster1-4brtq.mongodb.net/conduit?retryWrites=true&w=majority", { useNewUrlParser: true })
   mongoose.set('debug', true);
 }
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 require('./models/User');
 require('./models/Article');
 require('./models/Comment');
